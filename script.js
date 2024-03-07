e4c1f18e50e26b16ca80f6ac0cecb24ff2728ab5
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-07 17:28:44 +0500

    All functions added for text analyze project

--- script.js
+++ script.js
@@ -2,4 +2,20 @@ console.log("Connection successfully!");
 
 function Analyze() { 
     console.log("Analyzing text");
+
+    let text = document.getElementById("text").value;
+    console.log(text);  
+
+    let words;
+    text = text.trim();
+    if(text.trim() === "") {
+        words = [];
+    } else {
+        words = text.split(" ");    
+    }
+
+    console.log(words.length);
+    document.getElementById("words").innerHTML = "Count of words : " + words.length; 
+    document.getElementById("textLength").innerHTML = "Length of text : " + text.length; 
+    document.getElementById("averageWordLength").innerHTML = "Average word length : " + Math.floor(text.length/words.length); 
 }
